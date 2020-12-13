98034f50afab0be2c054e4c08b8ce5f4a7cb4e88
Your Name <nntwari75@gmail.com>
2020-12-13 13:00:18 -0800

    adding more javaScript codes

--- Desktop/moringa-school-projects/moringa-project-week2/script.js
+++ Desktop/moringa-school-projects/moringa-project-week2/script.js
@@ -1,4 +1,4 @@
-function(){
+function myfunction(){
 	var date = document.getElementById("input-date").value;
 	var DD,MM,CC,YY;
 	DD=parseInt(date.slice(0,2));
@@ -7,16 +7,17 @@ function(){
 	YY=parseInt(date.slice(8,10));
 	var weekday= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
 
-    
+    var male_gender=document.getElementById("form-check male");
+    var female_gender=document.getElementById("form-check female");
 
-	var male = [kwadwo,kwabena,kwaku,yaw,kofi,kwame,kwasi]
+	var male = [kwadwo,kwabena,kwaku,yaw,kofi,kwame,kwasi];
 	var female=[adwoa,adwoa,akua,yaa,afua,ama,akosua]
-	if( male===true && result=1{alert (male[0]);})else if(male===true &&result=2{alert(male[1]);})
-	else if (male===true &&result=3){alert(male[2]); }else if (male===true &&result=4){alert(male[3]);}
-	else if (male===true&&result=5) {alert(male[4]);}else if (male===true&&result=6) {alert(male[5]);}
-	else if(male===true&&result=7){alert(male[6]);}else if( male===false && result=1{alert (female[0]);})else if(male===false &&result=2{alert(female[1]);})
-	else if (male===false &&result=3){alert(female[]2); }else if (male===false &&result=4) {alert(female[3]);}
-	else if (male===false&&result=5) {alert(female[4]);}else if (male===false&&result=6) {alert(female[5]);}
-	else if(male===false&&result=7){alert(female[6]);}
+	if(male.checked==true && weekday==1{alert(male[0]);})else if(male.checked==true &&weekday==2{alert(male[1]);})
+	else if (male.checked==true &&weekday==3){alert(male[2]); }else if (male.checked==true &&weekday==4){alert(male[3]);}
+	else if (male.checked==true&&weekday==5) {alert(male[4]);}else if (male.checked==true&&weekday==6) {alert(male[5]);}
+	else if(male.checked==true&&weekday==7){alert(male[6]);}else if( female.checked==true && weekday==1{alert (female[0]);})else if(female.checked==true &&weekday==2{alert(female[1]);})
+	else if (female.checked==true &&weekday==3){alert(female[2]); }else if (female.checked==true &&weekday==4) {alert(female[3]);}
+	else if (female.checked==true&&weekday==5) {alert(female[4]);}else if (female==true&&weekday==6) {alert(female[5]);}
+	else (female.checked==true&&weekday==7){alert(female[6]);}
 
 }
