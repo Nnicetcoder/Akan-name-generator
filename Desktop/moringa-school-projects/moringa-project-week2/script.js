93f6ee2a4ee8662e46e0f481ef10dd9f62b3bc07
Your Name <nntwari75@gmail.com>
2020-12-13 04:57:03 -0800

    adding js codes

---
+++ Desktop/moringa-school-projects/moringa-project-week2/script.js
@@ -0,0 +1,6 @@
+var date = document.getElementById("input-date");
+var DD,MM,CC,YY;
+DD=parseInt(date.slice(0,2));
+MM=parseInt(date.slice(3,5));
+CC=parseInt(date.slice(6,8));
+YY=parseInt(date.slice(8,10));
