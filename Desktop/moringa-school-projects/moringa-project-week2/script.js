d533ea9d8766cf5c2ee9580a8a205c69bcbf221f
Your Name <nntwari75@gmail.com>
2020-12-13 05:02:54 -0800

    adding more javaScript codes

--- Desktop/moringa-school-projects/moringa-project-week2/script.js
+++ Desktop/moringa-school-projects/moringa-project-week2/script.js
@@ -4,3 +4,4 @@ DD=parseInt(date.slice(0,2));
 MM=parseInt(date.slice(3,5));
 CC=parseInt(date.slice(6,8));
 YY=parseInt(date.slice(8,10));
+var weekday= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
